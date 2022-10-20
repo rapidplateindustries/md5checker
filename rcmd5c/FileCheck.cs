7154726d57662a0374e886b06c4a11fd4b018439
australisalchemy <r3df411@gmail.com>
2022-10-21 00:49:14 +1100

    upload project
    checks a certain files md5 checksum via command line arguments in a console window. (2012)

---
+++ rcmd5c/FileCheck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace rcmd5c
+{
+    public class FileCheck
+    {
+
+        /// <summary>
+        /// Calculates a files MD5 Checksum
+        /// </summary>
+        /// <param name="fileName">The file that is being checked</param>
+        /// <returns>(string) The MD5 Checksum of the file</returns>
+        public string GetMD5Checksum(string fileName)
+        {
+            // Initialize variables, assign them and what not
+            FileStream fStream = new FileStream(fileName, FileMode.Open);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] returnValue = md5.ComputeHash(fStream);
+            
+            // Close the FileStream (fStream)
+            fStream.Close();
+
+            // Create a string builder
+            StringBuilder strBuilder = new StringBuilder();
+
+            // Calculate the MD5
+            for (int i = 0; i < returnValue.Length; i++)
+            {
+                strBuilder.Append(returnValue[i].ToString("x2"));
+            }
+
+            // Return the StringBuilder.ToString()
+            return strBuilder.ToString();
+        }
+    }
+}
---
+++ rcmd5c/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rcmd5c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCheck fCheck = new FileCheck();
+
+            Console.WriteLine("MD5 Checksum of: {0}\n{1}\n", args[0], fCheck.GetMD5Checksum(args[0]));
+            Console.Read();
+        }
+    }
+}
